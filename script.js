11cc4800dcb8f3165b08e2755c92fd63c03a1e94
Allen Huffman <huffmancharles14@gmail.com>
2023-10-14 10:21:00 -0500

    Add event to choose player buttons, add player array to gameboard object, start on playGame function.

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
+const chooseClass = document.getElementById("class-btns");
+let player1;
+let player2;
+
 const Gameboard = (function () {
   const board = ["", "", "", "", "", "", "", "", ""];
+  const player = [];
 
-  return { board };
+  return { board, player };
 })();
 
 function createPlayer(playerName, playerMark) {
@@ -13,6 +18,40 @@ function createPlayer(playerName, playerMark) {
     },
   };
 }
+
+const playGame = (function () {
+  // Handle gameplay here.
+  // We will set innderText for each cell with X's and O's here.
+  // We will also checkFor winner each time we place a mark.
+  //
+})();
+
+chooseClass.addEventListener("click", (event) => {
+  if (event.target.id === "player-1") {
+    //here we will call the createPlayer function, set playerName to
+    //player 1 with mark X
+    if (!player1) {
+      player1 = createPlayer("player 1", "X");
+
+      Gameboard.player.push(player1);
+      console.log(Gameboard.player);
+      console.log(player1.getPlayer());
+    } else {
+      alert("Player 1, (X), has already been chosen!");
+    }
+  } else if (event.target.id === "player-2") {
+    //also call the createPlayer function, set playerName to
+    //player 2 with mark O
+    if (!player2) {
+      player2 = createPlayer("Player 2!", "O");
+      Gameboard.player.push(player2);
+      console.log(Gameboard.player);
+      console.log(player2.getPlayer());
+    } else {
+      alert("Player 2, (O), has already been chosen!");
+    }
+  }
+});
 
 /* 
 TicTacToe
